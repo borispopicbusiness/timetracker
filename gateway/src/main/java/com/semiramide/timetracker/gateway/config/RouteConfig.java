2626e3df30b095f999d499eb4f42be6198207c17
Boris Popic <borispopic.business@gmail.com>
2025-04-19 18:51:41 +0200

    refactor: apply basic cleanup across the project

--- gateway/src/main/java/com/semiramide/timetracker/gateway/config/RouteConfig.java
+++ gateway/src/main/java/com/semiramide/timetracker/gateway/config/RouteConfig.java
@@ -8,24 +8,22 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RouteConfig {
-
-  @Bean
-  public RouteLocator myRoutes(RouteLocatorBuilder builder, GatewayProperties props) {
-    return builder
-        .routes()
-        .route(
-            "backend_service",
-            p ->
-                p.path(
-                        "/project/**",
-                        "/employee/**",
-                        "/worklog/**",
-                        "/leave/**",
-                        "/project-employees/**",
-                        "/export/**",
-                        "/test/**")
-                    .filters(f -> f.prefixPath("/backend_service"))
-                    .uri("http://" + props.getBackendHost() + "/"))
-        .build();
-  }
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder, GatewayProperties props) {
+        return builder
+                .routes()
+                .route("backend_service",
+                        p ->
+                                p.path(
+                                                "/project/**",
+                                                "/employee/**",
+                                                "/worklog/**",
+                                                "/leave/**",
+                                                "/project-employees/**",
+                                                "/export/**",
+                                                "/test/**")
+                .filters(f -> f.prefixPath("/backend_service"))
+                .uri("http://" + props.getBackendHost() + "/"))
+                .build();
+    }
 }
